3588e29f4ba6c0a892824008b5c880f6ec1ba00a
abdulwasih <vasih95@gmail.com>
2021-10-20 17:30:13 +0530

    edit

--- companyportal/settings.py
+++ companyportal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3@c3xl+ldeavab-+g@&ytcba8x5x+@+-=^enhsoj4^@@wk@k@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abcsolutions.herokuapp.com','localhost']
 
 
 # Application definition
